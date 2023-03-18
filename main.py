691190d83e033509d357da5705e2074309997fb8
samyar89 <128214024+samyar89@users.noreply.github.com>
2023-03-18 11:34:11 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-numbers = [] # لیستی برای ذخیره اعداد وارد شده
- 
-n = int(input("تعداد اعداد مورد نظر را وارد کنید: ")) # دریافت تعداد اعداد از کاربر
- 
-for i in range(n):
-    number = int(input("عدد بعدی را وارد کنید: ")) 
-    numbers.append(number) # اضافه کردن هر اعداد وارد شده به لیست
-   
-average = sum(numbers)/n # حساب میانگین
- 
-print("میانگین اعداد وارد شده برابر است با:", average)
+from tkinter import *
+
+root = Tk()
+
+root.geometry("400x350")
+root.title("محاسبه میانگین")
+
+def average():
+    numbers = number_entry.get()
+    int_numbers = list(map(int, numbers.split(",")))
+    
+    avg = sum(int_numbers) / len(int_numbers)
+    
+    result_label.config(text=f"میانگین: {avg}")
+
+number_label = Label(root, text="لطفا اعداد خود را با کاما جدا کنید:")
+number_label.pack(pady=10)
+
+number_entry = Entry(root, width=40)
+number_entry.pack(pady=5)
+
+calculate_button = Button(root, text="محاسبه میانگین", command=average)
+calculate_button.pack(pady=10)
+
+result_label = Label(root, text="")
+result_label.pack()
+
+root.mainloop()
